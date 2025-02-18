3bc392b0ef536871b2e1930d047b519aca4cc16b
Flávio Henrique <flaviohps8@gmail.com>
2025-02-18 17:38:49 -0300

    🎉 Dia 1
    Código completo referente ao primeiro dia do evento

---
+++ src/components/icon-button.tsx
@@ -0,0 +1,12 @@
+import type { ComponentProps, ReactNode } from "react"
+
+interface IconButtonProps extends ComponentProps<"button"> {}
+
+export default function IconButton(props: IconButtonProps) {
+    return (
+        <button
+            className="p-1.5 bg-gray-500 text-blue outline-0 rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+            {...props}
+        />
+    )
+}
